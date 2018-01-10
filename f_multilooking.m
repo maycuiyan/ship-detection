13053544e02d0877ca74d42d718933ab741001fb
unknown <cuiyi.trea@gmail.com>
2018-01-10 12:39:14 -0800

    updated annotation

--- f_multilooking.m
+++ f_multilooking.m
@@ -1,11 +1,16 @@
 function I_multilook = f_multilooking(I,az_look,rg_look)
-
-N_az = floor(size(I,1)/az_look);
-N_rg = floor(size(I,2)/rg_look);
-I_multilook = zeros(N_az,N_rg);
-for i = 1:N_az
-%     i
-    for j = 1:N_rg
-        I_multilook(i,j) = mean2(I((i-1)*az_look+1:i*az_look,(j-1)*rg_look+1:j*rg_look));
-    end
+    % This fucnction applies averaging pooling to the input image
+    % In radar imaging community, this is called 'multilooking'
+    % INPUTS:
+    %   I:          2D matrix, input SAR intensity image
+    %   az_look:    int, pool size in horizontal direction
+    %   rg_look:    int, pool size in vertical direction
+    N_az = floor(size(I,1)/az_look);
+    N_rg = floor(size(I,2)/rg_look);
+    I_multilook = zeros(N_az,N_rg);
+    for i = 1:N_az
+        for j = 1:N_rg
+            I_multilook(i,j) = mean2(I((i-1)*az_look+1:i*az_look,(j-1)*rg_look+1:j*rg_look));
+        end
+    end 
 end
